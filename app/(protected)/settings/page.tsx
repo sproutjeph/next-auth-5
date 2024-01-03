2feed823723b46095f47ae5961640ec7bb7a19fd
jephthah <jephthah.mbah@outlook.com>
2024-01-03 20:54:42 +0100

    added Signout Logic

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,11 +1,25 @@
-import { auth } from "@/auth";
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
 import { FC } from "react";
 
 interface SettingsPageProps {}
 
 const SettingsPage: FC<SettingsPageProps> = async ({}) => {
   const session = await auth();
-  return <div>{JSON.stringify(session)}</div>;
+  return (
+    <div>
+      {JSON.stringify(session)}
+
+      <form
+        action={async () => {
+          "use server";
+          await signOut();
+        }}
+      >
+        <Button type="submit">Sign Out</Button>
+      </form>
+    </div>
+  );
 };
 
 export default SettingsPage;
