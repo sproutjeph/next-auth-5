e4eda7f1f55520b8ab3304d08f6013be41aea78c
jephthah <jephthah.mbah@outlook.com>
2024-01-04 20:39:15 +0100

    added github auth provider ids

--- auth.config.ts
+++ auth.config.ts
@@ -4,9 +4,16 @@ import type { NextAuthConfig } from "next-auth";
 import { LoginSchema } from "./schemas";
 import { getUserByEmail } from "./data/user";
 import bcrypt from "bcryptjs";
+import Google from "next-auth/providers/google";
+import Github from "next-auth/providers/github";
 
 export default {
   providers: [
+    Google,
+    Github({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
     Credentials({
       async authorize(credentials) {
         const validatedFields = LoginSchema.safeParse(credentials);
