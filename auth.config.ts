7ad5c988291b1e612ea4e25dba0181ca14483377
jephthah <jephthah.mbah@outlook.com>
2024-01-04 20:55:36 +0100

    added google api ids

--- auth.config.ts
+++ auth.config.ts
@@ -9,7 +9,10 @@ import Github from "next-auth/providers/github";
 
 export default {
   providers: [
-    Google,
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
     Github({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
