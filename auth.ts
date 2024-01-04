adafd5521f1b92e536ae4aff609664dc0de605cc
jephthah <jephthah.mbah@outlook.com>
2024-01-04 20:15:59 +0100

    added user role

--- auth.ts
+++ auth.ts
@@ -1,6 +1,8 @@
-import NextAuth from "next-auth";
-import authConfig from "./auth.config";
 import { PrismaAdapter } from "@auth/prisma-adapter";
+import { UserRole } from "@prisma/client";
+import { getUserById } from "./data/user";
+import authConfig from "./auth.config";
+import NextAuth from "next-auth";
 import { db } from "./lib/db";
 
 export const {
@@ -15,9 +17,23 @@ export const {
         session.user.id = token.sub;
       }
 
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+
       return session;
     },
     async jwt({ token }) {
+      if (!token.sub) {
+        return token;
+      }
+      const exsitingUser = await getUserById(token.sub);
+      if (!exsitingUser) {
+        return token;
+      }
+
+      token.role = exsitingUser.role;
+
       return token;
     },
   },
